a8c47c0becca4de69e1673a1ec415cd57cc824a4
Angel D. Munoz <scythes.lol@gmail.com>
2020-01-25 12:44:29 -0700

    add managed notifications

--- Counter.fs
+++ Counter.fs
@@ -1,59 +1,71 @@
 ﻿namespace NetworkDetection
 
 module Counter =
+    open System
     open System.Net.NetworkInformation
     open Avalonia.Controls
     open Avalonia.FuncUI.DSL
     open Avalonia.Layout
-    
-    type State = { count : int; isNetworkUp: bool }
-    let init = 
+    open Avalonia.Controls.Notifications
+
+    type State =
+        { count: int
+          isNetworkUp: bool
+          notificationManager: WindowNotificationManager }
+
+    let init notificationManager =
         let isDown =
             NetworkInterface.GetAllNetworkInterfaces()
-            |> Seq.where (fun network -> network.OperationalStatus |> int = 1)
+            |> Seq.where (fun network ->
+                network.OperationalStatus
+                |> int = 1)
             |> Seq.isEmpty
-        { isNetworkUp = not isDown; count = 0 }
+        { isNetworkUp = not isDown
+          count = 0
+          notificationManager = notificationManager }
 
-    type Msg = Increment | Decrement | Reset | NetworkChanged of bool
+    type Msg =
+        | Increment
+        | Decrement
+        | Reset
+        | NetworkChanged of bool
 
-    let update (msg: Msg) (state: State) : State =
+    let update (msg: Msg) (state: State): State =
         match msg with
         | Increment -> { state with count = state.count + 1 }
         | Decrement -> { state with count = state.count - 1 }
-        | Reset -> init
-        | NetworkChanged isUp -> { state with isNetworkUp = isUp }
-    
+        | Reset -> init state.notificationManager
+        | NetworkChanged isUp -> 
+            let message = sprintf "Network status is %s" (if isUp then "Up" else "Down")
+            let time = Nullable(TimeSpan.FromSeconds 3.5)
+            let notification = Notification("Network Changed", message, NotificationType.Information, time)
+            state.notificationManager.Show(notification)
+            { state with isNetworkUp = isUp }
+
     let view (state: State) (dispatch) =
-        DockPanel.create [
-            DockPanel.children [
-                Button.create [
-                    Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> dispatch Reset)
-                    Button.content "reset"
-                ]                
-                Button.create [
-                    Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> dispatch Decrement)
-                    Button.content "-"
-                ]
-                Button.create [
-                    Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> dispatch Increment)
-                    Button.content "+"
-                ]
-                TextBlock.create [
-                    TextBlock.dock Dock.Top
-                    TextBlock.fontSize 32.0
-                    TextBlock.verticalAlignment VerticalAlignment.Center
-                    TextBlock.horizontalAlignment HorizontalAlignment.Center
-                    TextBlock.text (sprintf "Network is %s" (if state.isNetworkUp then "Up" else "Down"))
-                ]
-                TextBlock.create [
-                    TextBlock.dock Dock.Top
-                    TextBlock.fontSize 48.0
-                    TextBlock.verticalAlignment VerticalAlignment.Center
-                    TextBlock.horizontalAlignment HorizontalAlignment.Center
-                    TextBlock.text (string state.count)
-                ]
-            ]
-        ]       
+        DockPanel.create
+            [ DockPanel.children
+                [ Button.create
+                    [ Button.dock Dock.Bottom
+                      Button.onClick (fun _ -> dispatch Reset)
+                      Button.content "reset" ]
+                  Button.create
+                      [ Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> dispatch Decrement)
+                        Button.content "-" ]
+                  Button.create
+                      [ Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> dispatch Increment)
+                        Button.content "+" ]
+                  TextBlock.create
+                      [ TextBlock.dock Dock.Top
+                        TextBlock.fontSize 32.0
+                        TextBlock.verticalAlignment VerticalAlignment.Center
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (sprintf "Network is %s" (if state.isNetworkUp then "Up" else "Down")) ]
+                  TextBlock.create
+                      [ TextBlock.dock Dock.Top
+                        TextBlock.fontSize 48.0
+                        TextBlock.verticalAlignment VerticalAlignment.Center
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (string state.count) ] ] ]
--- Program.fs
+++ Program.fs
@@ -8,37 +8,38 @@ open Avalonia.FuncUI.Elmish
 open Avalonia.FuncUI.Components.Hosts
 open System.Net.NetworkInformation
 open Avalonia.Threading
+open Avalonia.Controls.Notifications
 
 type MainWindow() as this =
     inherit HostWindow()
     do
         base.Title <- "NetworkDetection"
-        base.Width <- 400.0
-        base.Height <- 400.0
-        
+        base.Width <- 800.0
+        base.Height <- 600.0
+
         //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
         //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
-        
+
         let networkStatus (initial: Counter.State) =
             let sub dispatch =
-                NetworkChange
-                    .NetworkAvailabilityChanged
-                    .Subscribe(fun args -> dispatch (Counter.Msg.NetworkChanged args.IsAvailable)) 
-                    |> ignore
+                NetworkChange.NetworkAvailabilityChanged.Subscribe
+                    (fun args -> dispatch (Counter.Msg.NetworkChanged args.IsAvailable)) |> ignore
             Cmd.ofSub sub
 
-        let syncDispatch (dispatch: Dispatch<'msg>) : Dispatch<'msg> =
+        let syncDispatch (dispatch: Dispatch<'msg>): Dispatch<'msg> =
             match Dispatcher.UIThread.CheckAccess() with
-            | true -> fun msg -> Dispatcher.UIThread.Post (fun () -> dispatch msg)
+            | true -> fun msg -> Dispatcher.UIThread.Post(fun () -> dispatch msg)
             | false -> fun msg -> dispatch msg
 
-        Elmish.Program.mkSimple (fun () -> Counter.init) Counter.update Counter.view
+        let notificationManager = WindowNotificationManager(this)
+
+        Elmish.Program.mkSimple (fun () -> (Counter.init notificationManager)) Counter.update Counter.view
         |> Program.withHost this
         |> Program.withSyncDispatch syncDispatch
         |> Program.withSubscription networkStatus
         |> Program.withConsoleTrace
         |> Program.run
-        
+
 type App() =
     inherit Application()
 
@@ -48,16 +49,11 @@ type App() =
 
     override this.OnFrameworkInitializationCompleted() =
         match this.ApplicationLifetime with
-        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
-            desktopLifetime.MainWindow <- MainWindow()
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime -> desktopLifetime.MainWindow <- MainWindow()
         | _ -> ()
 
 module Program =
 
     [<EntryPoint>]
-    let main(args: string[]) =
-        AppBuilder
-            .Configure<App>()
-            .UsePlatformDetect()
-            .UseSkia()
-            .StartWithClassicDesktopLifetime(args)
+    let main (args: string []) =
+        AppBuilder.Configure<App>().UsePlatformDetect().UseSkia().StartWithClassicDesktopLifetime(args)
